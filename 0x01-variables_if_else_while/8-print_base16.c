5ede879cce466c82c0b767f74a8799581d7f0b9b
Leen <simplyleen2@gmail.com>
2023-06-16 15:52:06 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,14 @@
 int main(void)
 {
 	int i;
-	for (i = 0; i <= 255; i++)
+
+	for (i = 48; i < 58; i++)
 	{
-		putchar(i < 10 ? i + '0' : i - 10 + 'a');
+		putchar(i);
+	}
+	for (i=97; i < 103; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
