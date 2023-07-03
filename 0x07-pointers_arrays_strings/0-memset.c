b5aefcdeecedbee27bf667e4937705dc7e665744
Leen <simplyleen2@gmail.com>
2023-07-03 05:54:23 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
+
 	unsigned int i;
 
-	while (*s != 0)
-	{
-		for (i = 0; i < n; i++)
-		{
-			*s = b;
-		}
-	}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
