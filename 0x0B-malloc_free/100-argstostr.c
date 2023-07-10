53f04fe25e3e60e17da359b4dfc0f7ff87687211
Leen <simplyleen2@gmail.com>
2023-07-10 15:02:51 -0700

    0x0B

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - function that concatenates all the arguments of your program.
+ * @ac: number of command line arguments
+ * @av: is an array of pointers to command-line
+ * Return: pointer to new string.
+ * NULL if it fails, or if ac == 0, or av == NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, n, r = 0, l = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+			l++;
+	}
+	l += ac;
+
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (n = 0; av[i][n]; n++)
+	{
+		str[r] = av[i][n];
+		r++;
+	}
+	if (str[r] == '\0')
+	{
+		str[r++] = '\n';
+	}
+	}
+	return (str);
+}
+
