ec13219193796b706075e77ce656c27cc1ff6472
Leen <simplyleen2@gmail.com>
2023-07-10 09:54:33 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,9 +22,15 @@ char *str_concat(char *s1, char *s2)
 		strcat(nspace, s2);
 	}
 	else if (s1 != NULL)
+	{
 		strcpy(nspace, s1);
+		strcat(nspace, "\0");
+	}
 	else if (s2 != NULL)
+	{
 		strcpy(nspace, s2);
+		strcat(nspace, "\0");
+	}
 	else
 		return (NULL);
 
