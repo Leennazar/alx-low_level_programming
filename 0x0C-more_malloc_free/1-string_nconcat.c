c4218a58f86487d1fd7dbce502488070914a715e
Leen <simplyleen2@gmail.com>
2023-07-13 08:15:09 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	else
 	{
-		ptr = malloc(sizeof(char) * (len1 + n));
+		ptr = malloc(sizeof(char) * (len1 + n - 1));
 	}
 	if (ptr == NULL)
 	{
