ca321d2a8bec7080d458cdc8d77aa07222f8a868
Leen <simplyleen2@gmail.com>
2023-07-13 10:25:11 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
+	{
 		new_ptr = malloc(new_size);
+		if (new_ptr == NULL)
+			return (NULL);
+		return (new_ptr);
+	}
 	if ((new_size == 0) && (ptr != NULL))
 	{
 		free(ptr);
