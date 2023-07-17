88970c68c365ad4c6d4e2e89236a285c52ffc295
Leen <simplyleen2@gmail.com>
2023-07-17 08:44:34 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - data of dog
+  * @name: pointer to dog name
+  * @age: age of dog
+  * @owner: pointer to dog owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
