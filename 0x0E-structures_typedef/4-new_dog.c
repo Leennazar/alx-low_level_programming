28e2d9611a8442fcc5701a304c62e7c85ffd150a
Leen <simplyleen2@gmail.com>
2023-07-17 13:19:04 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
+        if (new_dog == NULL)
+                return (NULL);
 
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
@@ -30,7 +30,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
-		free(new_dog);
 		return (NULL);
 	}
 	return (new_dog);
