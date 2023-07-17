186e8595f29349a57ae3bbe12cd5ab01336b10f0
Leen <simplyleen2@gmail.com>
2023-07-17 13:44:57 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,72 @@
-#include "dog.h"
-#include <stddef.h>
-#include <string.h>
 #include <stdlib.h>
+#include "dog.h"
+
+
 /**
- * new_dog - function that creates a new dog.
- * @name: new name pointer
- * @age: new dogs age
- * @owner: new owner pointer
- * Return: A pointer to the new dog, or NULL if the function fails
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+
+	i = 0;
+
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+
+	return (i);
+}
+
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+
+	len = 0;
+
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+
+	return (dest);
+}
+
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -44,4 +103,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
+
+
 
