c6be2d13d6dee21bc98f37ea18ed347a5eb789af
Leen <simplyleen2@gmail.com>
2023-07-19 16:07:27 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "3-calc.h"
 /**
- * main - prints its own opcode
+ * main - program that prints opcodes for its own main function
  * @argc: argument count
  * @argv: argument vector
- * Return: Always 0 (Success)
+ * Return: always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-	int bytes, x;
+	int x, bytes;
 	char *arr;
 
 	if (argc != 2)
@@ -19,18 +17,20 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	bytes = atoi(argv[1]);
+
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 	arr = (char *)main;
+
 	x = 0;
 	while (x < bytes - 1)
 	{
 		printf("%02hhx", arr[x]);
 		x++;
 	}
-	printf("%02hhx\n", arr[x]);
+	printf("%02hhx\n", array[x]);
 	return (0);
 }
