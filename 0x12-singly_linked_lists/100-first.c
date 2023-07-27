09db60271a652ef06d170e42c453ff61077d2bb7
Leen <simplyleen2@gmail.com>
2023-07-27 13:32:29 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * before_main - function that prints You're beat! and yet, you must allow,\n
+ * I bore my house upon my back!\n before the main function is executed.
+ * Return: sentence
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
