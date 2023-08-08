4ce634f2c9de7aa00767c33a09bc4da323dc93ff
leennazar <simplyleen2@gmail.com>
2023-08-09 01:40:22 +0200

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 #define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file%s\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
 #define ERR_NOWRITE "Error: Can't write to %s\n"
 #define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
@@ -31,6 +31,7 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (b == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
+
 	from_fd = close(from_fd);
 	to_fd = close(to_fd);
 	if (from_fd)
